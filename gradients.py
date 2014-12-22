8d7fafd42037007eb94700ee74708454d136634c
James Durrant <jdurrant14@gmail.com>
2014-12-22 00:09:06 +0000

    Changed variance algorithm to remove numerical instability according to http://en.wikipedia.org/wiki/Algorithms_for_calculating_variance

--- gradients.py
+++ gradients.py
@@ -3,13 +3,6 @@ import numpy as np
 from gco_python_master.pygco import cut_simple
 from scipy import misc, ndimage
 
-image_A = misc.imread('A.png')[:,:,0:3]
-image_B = misc.imread('B.png')[:,:,0:3]
-h, w, dim = image_A.shape
-
-all_paths = generate_all_paths(-15, 15)
-
-
 def generate_all_paths(min_displacement, max_displacement):
     range_displacement = max_displacement - min_displacement + 1
     all_paths = np.zeros((range_displacement*range_displacement, 2), 'int16')
@@ -31,13 +24,41 @@ def matching_cost(pA, pB, A, B, gradient_A, gradient_B, variance_A, variance_B, 
 
     return np.power(np.true_divide(np.power(q, 2), variance_A[Ay, Ax] * variance_B[By, Bx]), beta)
 
-def variance_4_neighbourhood(A):
-    kernel = np.array([[0,1,0],[1,1,1],[0,1,0]])
+def variance_4_neighbourhood(values):
+    # numerically stable variance by using shifted data
+    h, w, dim = values.shape
+    variance = np.zeros((h,w), dtype='int32')
+    for y in range(0, h):
+        for x in range(0, w):
+            K = values[y,x]
+            n = 1
+            sum_neighbourhood = 0
+            sum_squared_neighbourhood = 0
+            if y > 0:
+                n += 1
+                sum_neighbourhood += np.sum(np.subtract(values[y - 1, x, :], K))
+                sum_squared_neighbourhood += np.sum(np.power(np.subtract(values[y - 1, x, :], K), 2))
+            if y < (h - 1):
+                n += 1
+                sum_neighbourhood += np.sum(np.subtract(values[y + 1, x, :], K))
+                sum_squared_neighbourhood += np.sum(np.power(np.subtract(values[y + 1, x, :], K), 2))
+            if x > 0:
+                n += 1
+                sum_neighbourhood += np.sum(np.subtract(values[y, x - 1, :], K))
+                sum_squared_neighbourhood += np.sum(np.power(np.subtract(values[y, x - 1, :], K), 2))
+            if x < (w - 1):
+                n += 1
+                sum_neighbourhood += np.sum(np.subtract(values[y, x + 1, :], K))
+                sum_squared_neighbourhood += np.sum(np.power(np.subtract(values[y, x + 1, :], K), 2))
+            variance[y, x] = np.true_divide(sum_squared_neighbourhood - np.true_divide(np.power(sum_neighbourhood, 2), n), n)
+    return variance
 
-    number_neighbours = ndimage.convolve(np.sum(np.ones_like(A), axis=2), kernel, mode='constant', cval=0)
-    sum_neighbours = ndimage.convolve(np.sum(A, axis=2), kernel, mode='constant', cval=0)
-    mean_neighbours = np.true_divide(sum_neighbours, number_neighbours)
+def main():
+    image_A = misc.imread('A.png')[:,:,0:3]
+    image_B = misc.imread('B.png')[:,:,0:3]
+    h, w, dim = image_A.shape
 
-    sum_squared_neighbours = ndimage.convolve(np.sum(np.power(A.astype('int32'), 2), axis=2), kernel, mode='constant', cval=0)
+    all_paths = generate_all_paths(-15, 15)
 
-    return np.true_divide(sum_squared_neighbours, number_neighbours) - np.power(mean_neighbours, 2)
+if __name__ == "__main__":
+    main()
