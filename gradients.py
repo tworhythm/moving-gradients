72922636e3b879760bf496cecac18f64d70b6bc9
James Durrant <jdurrant14@gmail.com>
2015-01-24 23:38:18 +0000

    Added in output of intermediate images by using bilinear interpolation along the paths

--- gradients.py
+++ gradients.py
@@ -160,6 +160,41 @@ def calculate_pairwise_costs(all_paths, delta, smooth_costs):
             smooth_costs[i,j] = min(np.sqrt(np.power((all_paths[i,0] + all_paths[i,2]) - (all_paths[j,0] + all_paths[j,2]),2) + np.power((all_paths[i,1] + all_paths[i,3]) - (all_paths[j,1] + all_paths[j,3]),2)), delta)
             smooth_costs[j,i] = smooth_costs[i,j]
 
+def generate_interpolated_frame(frame, image_A, image_B, result, all_paths, f, intermediate_frames):
+    for y in range(0, h):
+        for x in range(0, w):
+            path = all_paths[result[y,x], :]
+            full_path_length = np.sqrt(np.power(path[0] + path[2], 2) + np.power(path[1] + path[3], 2))
+            first_path_length = np.sqrt(np.power(path[0], 2) + np.power(path[1], 2))
+            if full_path_length <= np.spacing(1):
+                transition_point = 0
+            elif full_path_length == first_path_length:
+                transition_point = 0
+            else:
+                transition_point = np.true_divide(first_path_length, full_path_length)
+            if transition_point <= np.true_divide(f, intermediate_frames - 1):
+                image = image_A
+                y_interp = y + np.true_divide(f, intermediate_frames - 1) * (path[0] + path[2])
+                x_interp = x + np.true_divide(f, intermediate_frames - 1) * (path[1] + path[3])
+            else:
+                image = image_B
+                y_interp = y - np.true_divide(f, intermediate_frames - 1) * (path[0] + path[2])
+                x_interp = x - np.true_divide(f, intermediate_frames - 1) * (path[1] + path[3])
+            frame[y, x] = bilinear_interp(image, y_interp, x_interp)
+
+def bilinear_interp(image, y, x):
+    xmin = np.floor(x)
+    xmax = np.ceil(x)
+    ymin = np.floor(y)
+    ymax = np.ceil(y)
+
+    b1 = image[ymin, xmin]
+    b2 = image[ymin, xmax] - image[ymin, xmin]
+    b3 = image[ymax, xmin] - image[ymin, xmin]
+    b4 = image[ymin, xmin] - image[ymin, xmax] - image[ymax, xmin] + image[ymax, xmax]
+
+    return b1 + b2*(x - xmin) + b3*(y - ymin) + b4*(x - xmin)*(y - ymin)
+
 # def main():
 print("Initial setup...")
 start = timeit.default_timer()
@@ -230,6 +265,15 @@ print("Assigning labels...")
 start = timeit.default_timer()
 unaries_int = unaries.astype('int32')
 result = cut_simple(unaries_int, smooth_costs_int)
+stop = timeit.default_timer()
+print("[%.4fs]" % (stop - start))
+
+print("Interpolate images...")
+start = timeit.default_timer()
+frame = np.zeros_like(image_A)
+for f in range(0, intermediate_frames):
+    generate_interpolated_frame(frame, image_A, image_B, result, all_paths, f, intermediate_frames)
+    misc.imsave('output/%02d.png' % f, frame)
 stop = timeit.default_timer()
 print("[%.4fs]" % (stop - start))
 
